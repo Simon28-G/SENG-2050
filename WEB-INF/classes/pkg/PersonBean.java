51b91db72333a3cd8ed0381f236744a38852127f
Nathan Vilmen <nathanvilmen@hotmail.fr>
2023-05-25 22:48:00 +1000

    Added UserBean and PersonBean structure

--- WEB-INF/classes/pkg/PersonBean.java
+++ WEB-INF/classes/pkg/PersonBean.java
@@ -1,5 +1,74 @@
 package pkg;
 
 public class PersonBean {
+
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String contactNumber;
+    
+
+    public PersonBean() {
+        
+    }
+
+
+
+    // Username
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    // Password
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    // First Name
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    // Last Name
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    // Email
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    // Contact Number
+    public String getContactNumber() {
+        return contactNumber;
+    }
+    
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
     
 }
--- WEB-INF/classes/pkg/UserBean.java
+++ WEB-INF/classes/pkg/UserBean.java
@@ -1,5 +1,65 @@
 package pkg;
 
-public class UserBean {
+import java.util.ArrayList;
+
+/**
+ * Bean representing a specific user.
+ */
+public class UserBean extends PersonBean{
     
+    /**
+     * Unique ID number identifying the user.
+     */
+    private int userId;
+
+
+    public UserBean() {
+        
+    }
+
+
+
+    public IssueBean reportIssue(String title, String description,
+            String category, ArrayList<String> keywords,
+            ArrayList<InputFileBean> filesAndImages) {
+
+        IssueBean issue = new IssueBean();
+
+        return issue;
+    }
+    
+
+
+    public IssueBean viewIssue(int issueId) {
+        
+        IssueBean issue = new IssueBean();
+
+        return issue;
+    }
+
+
+
+
+    public void addComment(int issueId, String content) {
+        
+    }
+
+
+
+    public void resolveIssue(int issueId) {
+        
+    }
+
+
+
+
+
+    public int getUserId() {
+        return this.userId;
+    }
+
+    public void setUserId(int id) {
+        this.userId = id;
+    }
+
 }
